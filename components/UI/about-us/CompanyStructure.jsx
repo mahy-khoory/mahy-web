64a95c2daef1aa16ca196ac9b428601111ab7c0c
muhammadhamzasalahuddin <mohammad.hamzaa.shahid@gmail.com>
2026-01-03 01:37:57 +0500

    data add

--- components/UI/about-us/CompanyStructure.jsx
+++ components/UI/about-us/CompanyStructure.jsx
@@ -61,7 +61,7 @@ function CompanyStructure() {
             <motion.div variants={containerVariants} initial="hidden" whileInView="visible" viewport={{ once: true }}>
                 <h2 className='font-bold text-2xl lg:text-4xl text-center uppercase'>Company Structure</h2>
             </motion.div>
-            <p className='text-2xl lgtext-3xl text-center font-medium mt-6 lg:mt-12 t-base'>Tencent</p>
+            <p className='text-2xl lgtext-3xl text-center font-medium mt-6 lg:mt-12 t-base'>MAHY KHOORY</p>
             <DesktopStructure items={items} itemVariants={itemVariants} containerVariants={containerVariants} />
             <MobileStructure items={items} itemVariants={itemVariants} />
         </section>
