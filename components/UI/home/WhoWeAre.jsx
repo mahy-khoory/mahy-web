56e233789a0e6cb779a3b9338c1348e9ff0cb1b5
Muhammad Bin Arshad <mbinac1199@gmail.com>
2026-01-04 17:20:32 +0500

    changes

--- components/UI/home/WhoWeAre.jsx
+++ components/UI/home/WhoWeAre.jsx
@@ -20,7 +20,7 @@ function WhoWeAre() {
         <section className='max-w-7xl mx-auto pt-15 md:pt-20'>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-18 md:h-[80vh]'>
                 <motion.div variants={container1} initial="hidden" whileInView="visible" viewport={{ once: true }}
-                    className='w-full px-5 md:w-120 md:px-0 flex flex-col justify-center'>
+                    className='w-full px-5 flex flex-col justify-center'>
                     <h2 className='t-base text-lg font-semibold tracking-tighter'>Who We Are</h2>
                     <p className='font-semibold text-4xl md:text-5xl mt-3 leading-12 md:leading-15'>Pioneering Transformation</p>
                     <p className='mt-5 md:mt-8 font-light text-gray-700'>The M.A.H.Y. Khoory Group is one of the UAE's most trusted business groups, delivering world-class technological solutions across diverse industries. Headquartered in Dubai, the group employs over 3,100 professionals from 30 nationalities, operates 17 offices across the UAE, and serves clients in more than 29 countries across the Middle East, the Levant, and Africa.</p>
