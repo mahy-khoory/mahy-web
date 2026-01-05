4d45546940891ad0d0342353d4a0861e6a5cdcf0
Muhammad Bin Arshad <mbinac1199@gmail.com>
2026-01-05 14:32:49 +0500

    changes

--- components/UI/home/CompaniesList.jsx
+++ components/UI/home/CompaniesList.jsx
@@ -1,9 +1,9 @@
 "use client"
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
-import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaArrowRightLong } from 'react-icons/fa6';
+import { motion, AnimatePresence } from "framer-motion";
 
 const tabs = ["Industeries", "Companies"];
 const industeries = [
@@ -15,13 +15,39 @@ const industeries = [
     { label: "Energy Transport", image: "/gallery/gallery-8.jpeg" },
 ];
 const moreIndusteries = ["Airports", "Data Center", "Defence & Airspace", "Road, Metro, & Rail"];
+const companies = [
+    { label: "MAHY Khoory Group", image: "/gallery/gallery-1.jpg" },
+    { label: "MAHY Khoory Trading", image: "/gallery/gallery-2.jpg" },
+    { label: "AKE", image: "/gallery/gallery-3.jpg" },
+    { label: "EIEME", image: "/gallery/gallery-4.jpg" },
+    { label: "GEMD", image: "/gallery/gallery-5.jpg" },
+    { label: "Al Mehwar", image: "/gallery/gallery-6.jpg" },
+    { label: "UwW", image: "/gallery/gallery-1.jpg" },
+    { label: "UPM", image: "/gallery/gallery-1.jpg" },
+    { label: "ALDPM", image: "/gallery/gallery-1.jpg" },
+    { label: "MKA", image: "/gallery/gallery-1.jpg" },
+    { label: "MKM", image: "/gallery/gallery-1.jpg" },
+    { label: "Greenland General Transport", image: "/gallery/gallery-1.jpg" },
+    { label: "RWMD", image: "/gallery/gallery-1.jpg" },
+    { label: "AL dhafra waste collection", image: "/gallery/gallery-1.jpg" },
+    { label: "Around continent", image: "/gallery/gallery-1.jpg" },
+    { label: "Ettihad waste management", image: "/gallery/gallery-1.jpg" },
+    { label: "Hote", image: "/gallery/gallery-1.jpg" },
+    { label: "Restaurant", image: "/gallery/gallery-1.jpg" },
+    { label: "NPI", image: "/gallery/gallery-1.jpg" },
+    { label: "Senan", image: "/gallery/gallery-1.jpg" },
+    { label: "Pure Energy", image: "/gallery/gallery-1.jpg" },
+    { label: "Clean Energy Solution", image: "/gallery/gallery-1.jpg" },
+    { label: "Union Sustainability", image: "/gallery/gallery-1.jpg" },
+    { label: "SWMD", image: "/gallery/gallery-1.jpg" }
+];
 
 function CompaniesList() {
     return (
-        <section className="max-w-7xl mx-auto pt-20 px-5 md:px-0">
+        <section className="max-w-7xl mx-auto pt-10 md:pt-20 px-5">
             <TabGroup>
                 <div className="flex justify-between flex-wrap gap-5">
-                    <h2 className='uppercase text-4xl font-semibold text-gray-700'>Businesses</h2>
+                    <h2 className='uppercase text-3xl md:text-4xl font-semibold text-gray-700'>Businesses</h2>
                     <div className='border-b border-gray-300 '>
                         <TabList className={"flex items-center gap-5"}>
                             {tabs.map((tab, i) => (
@@ -33,34 +59,55 @@ function CompaniesList() {
                         </TabList>
                     </div>
                 </div>
-                <TabPanels>
-                    <TabPanel className={"grid grid-cols-1 lg:grid-cols-2 gap-3 mt-10"}>
-                        <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
-                            {industeries.map((company, i) => (
+                <TabPanels className="mt-7 md:mt-10">
+                    <AnimatePresence mode="wait">
+                        <TabPanel
+                            key="industries"
+                            className="grid grid-cols-1 lg:grid-cols-2 gap-3"
+                            as={motion.div}
+                            initial={{ y: 10, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            viewport={{ once: true, margin: "-100px" }}
+                            transition={{ duration: 0.8, ease: "easeOut" }}
+                        >
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                                {industeries.map((industry, i) => (
+                                    <Card key={i} item={industry} />
+                                ))}
+                            </div>
+
+                            <div className="relative h-120 md:h-full">
+                                <Image src="/gallery/gallery-9.jpeg" alt="Companies" fill />
+                                <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent" />
+                                <div className='absolute bottom-7 left-7 text-gray-200'>
+                                    <div className='flex flex-wrap gap-3 mb-4'>
+                                        {moreIndusteries.map((item, i) => (
+                                            <Link href={"/"} key={i} className='bg-black/40 py-2 px-4 rounded-xl text-sm'>{item}</Link>
+                                        ))}
+                                    </div>
+                                    <Link href={"/"} className='border-b border-gray-200  pb-1'>Explore More</Link>
+                                </div>
+                            </div>
+                        </TabPanel>
+                        <TabPanel
+                            key="companies"
+                            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3"
+                            as={motion.div}
+                            initial={{ y: 10, opacity: 0 }}
+                            whileInView={{ y: 0, opacity: 1 }}
+                            viewport={{ once: true, margin: "-100px" }}
+                            transition={{ duration: 0.8, ease: "easeOut" }}
+                        >
+                            {companies.map((company, i) => (
                                 <Card key={i} item={company} />
                             ))}
-                        </div>
-                        <div className='relative h-120 md:h-full'>
-                            <Image src={"/gallery/gallery-9.jpeg"} alt='Companies' fill style={{ objectFit: "cover" }} />
-                            <div className='absolute inset-0 bg-linear-to-t from-black/30 to-transparent' />
-                            <div className='absolute bottom-7 left-7 text-gray-200'>
-                                <div className='flex flex-wrap gap-3 mb-4'>
-                                    {moreIndusteries.map((item, i) => (
-                                        <Link href={"/"} key={i} className='bg-black/40 py-2 px-4 rounded-xl text-sm'>{item}</Link>
-                                    ))}
-                                </div>
-                                <Link href={"/"} className='border-b border-gray-200  pb-1'>Explore More</Link>
-                            </div>
-                        </div>
-                    </TabPanel>
-                    <TabPanel className={"grid md:grid-cols-2 lg:grid-cols-3 gap-20 mt-20"}>
-
-                    </TabPanel>
+                        </TabPanel>
+                    </AnimatePresence>
                 </TabPanels>
             </TabGroup>
-        </section>
+        </section >
     )
-}
+};
 
 const Card = ({ item }) => (
     <div className="relative h-60 md:h-45 group overflow-hidden">
