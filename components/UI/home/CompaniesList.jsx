7128aeba713a8be98fdbcb1aebe9126c3556edb2
Muhammad Bin Arshad <mbinac1199@gmail.com>
2026-01-05 13:03:26 +0500

    changes

--- components/UI/home/CompaniesList.jsx
+++ components/UI/home/CompaniesList.jsx
@@ -2,10 +2,11 @@
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaArrowRightLong } from 'react-icons/fa6';
 
-const tabs = ["Companies", "Industeries"];
-const companies = [
+const tabs = ["Industeries", "Companies"];
+const industeries = [
     { label: "Trading", image: "/gallery/gallery-1.jpg" },
     { label: "Manufacturing", image: "/gallery/gallery-2.jpg" },
     { label: "Automobile", image: "/gallery/gallery-3.jpg" },
@@ -13,12 +14,13 @@ const companies = [
     { label: "Logistics", image: "/gallery/gallery-5.jpg" },
     { label: "Energy Transport", image: "/gallery/gallery-8.jpeg" },
 ];
+const moreIndusteries = ["Airports", "Data Center", "Defence & Airspace", "Road, Metro, & Rail"];
 
 function CompaniesList() {
     return (
-        <section className="max-w-7xl mx-auto pt-20">
+        <section className="max-w-7xl mx-auto pt-20 px-5 md:px-0">
             <TabGroup>
-                <div className="flex justify-between gap-5">
+                <div className="flex justify-between flex-wrap gap-5">
                     <h2 className='uppercase text-4xl font-semibold text-gray-700'>Businesses</h2>
                     <div className='border-b border-gray-300 '>
                         <TabList className={"flex items-center gap-5"}>
@@ -32,14 +34,23 @@ function CompaniesList() {
                     </div>
                 </div>
                 <TabPanels>
-                    <TabPanel className={"grid md:grid-cols-2 gap-3 mt-10"}>
-                        <div className='grid grid-cols-2 gap-3'>
-                            {companies.map((company, i) => (
-                                <Card item={company} />
+                    <TabPanel className={"grid grid-cols-1 lg:grid-cols-2 gap-3 mt-10"}>
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+                            {industeries.map((company, i) => (
+                                <Card key={i} item={company} />
                             ))}
                         </div>
-                        <div className='relative'>
+                        <div className='relative h-120 md:h-full'>
                             <Image src={"/gallery/gallery-9.jpeg"} alt='Companies' fill style={{ objectFit: "cover" }} />
+                            <div className='absolute inset-0 bg-linear-to-t from-black/30 to-transparent' />
+                            <div className='absolute bottom-7 left-7 text-gray-200'>
+                                <div className='flex flex-wrap gap-3 mb-4'>
+                                    {moreIndusteries.map((item, i) => (
+                                        <Link href={"/"} key={i} className='bg-black/40 py-2 px-4 rounded-xl text-sm'>{item}</Link>
+                                    ))}
+                                </div>
+                                <Link href={"/"} className='border-b border-gray-200  pb-1'>Explore More</Link>
+                            </div>
                         </div>
                     </TabPanel>
                     <TabPanel className={"grid md:grid-cols-2 lg:grid-cols-3 gap-20 mt-20"}>
@@ -52,7 +63,7 @@ function CompaniesList() {
 }
 
 const Card = ({ item }) => (
-    <div className="relative h-45 group overflow-hidden">
+    <div className="relative h-60 md:h-45 group overflow-hidden">
         <div className="absolute inset-0 transition-all duration-500 ease-out group-hover:scale-[1.3] group-hover:blur-xs">
             <Image src={item.image} alt={item.label} fill style={{ objectFit: "cover" }} />
         </div>
@@ -63,7 +74,7 @@ const Card = ({ item }) => (
                     <FaArrowRightLong size={20} color='white'
                         className='translate-x-40 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-500' />
                 </div>
-                <div className='h-px w-full bg-white mt-3 -translate-x-80 group-hover:translate-x-0 transition-all duration-500' />
+                <div className='h-px w-full bg-white mt-3 -translate-x-120 group-hover:translate-x-0 transition-all duration-500' />
             </div>
         </div>
     </div >
