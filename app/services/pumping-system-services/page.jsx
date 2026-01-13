e59c3625768881632126183923291733f76dc80a
muhammadhamzasalahuddin <mohammad.hamzaa.shahid@gmail.com>
2026-01-14 01:47:36 +0500

    pumping solution page

--- app/services/pumping-system-services/page.jsx
+++ app/services/pumping-system-services/page.jsx
@@ -12,7 +12,6 @@ function PumpingSystemServicesPage() {
                 description={"Reliable pumping system services delivering optimal performance and long-term efficiency. From installation to maintenance, we ensure smooth and uninterrupted operations."}
                 image={"/gallery/gallery-8.jpeg"}
             />
-            <PumpingServiceHorizontalScroll />
             <PumpingSystemServices />
             <PumpingServiceCards />
             <SplitScrollShowcase />
