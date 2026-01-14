f0f4a87c44a7046d7bfc7e99a816f16ccadba690
muhammadhamzasalahuddin <mohammad.hamzaa.shahid@gmail.com>
2026-01-14 14:56:07 +0500

    awards fixes

--- components/Awards/AwardsSection.jsx
+++ components/Awards/AwardsSection.jsx
@@ -4,22 +4,33 @@ import AnimatedLines from "../UI/AnimatedLines";
 import AwardsAccordion from "./AwardsAccordion";
 import { awardsData } from "./awardsData";
 
-
 export default function AwardsSection() {
-  const mid = Math.ceil(awardsData.length / 2);
+  const hasOdd = awardsData.length % 2 !== 0;
+  const mainItems = hasOdd ? awardsData.slice(0, -1) : awardsData;
+  const lastItem = hasOdd ? awardsData[awardsData.length - 1] : null;
+
+  const mid = Math.ceil(mainItems.length / 2);
 
   return (
-    <section className="bg-[#ffffff] py-24">
+    <section className="bg-white py-24">
       <div className="mx-auto max-w-7xl px-6">
         <AnimatedLines />
-        <h2 className="mb-16 text-center text-[38px] font-semibold text-[#2F3A40]">
-          Awards & Certifications
-        </h2>
 
+        <h2 className="mb-16 text-center text-[38px] font-semibold text-[#2F3A40]">
+          Certifications
+        </h2>
         <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-          <AwardsAccordion items={awardsData.slice(0, mid)} />
-          <AwardsAccordion items={awardsData.slice(mid)} />
+          <AwardsAccordion items={mainItems.slice(0, mid)} />
+          <AwardsAccordion items={mainItems.slice(mid)} />
         </div>
+
+        {lastItem && (
+          <div className="mt-6 flex justify-center">
+            <div className="w-full lg:max-w-[50%]">
+              <AwardsAccordion items={[lastItem]} />
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
--- components/Awards/awardsData.js
+++ components/Awards/awardsData.js
@@ -1,83 +1,92 @@
 export const awardsData = [
   {
     id: "iso-9001-das",
-    title: "ISO 9001:2015 Certification Awarded to DAS Holding",
+    title:
+      "ISO 9001:2015 Certification Awarded to Emirates International Equipment & Machinery L.L.C",
     description: [
-      "DAS Holding has been awarded ISO 9001:2015 Certification for its entire Management Services from QA International Certification Limited, U.K.",
-      "This highly regarded International Certification is a proof of our commitment to the highest standards of quality management and an affirmation of its leadership’s dedication to continual quality improvement.",
+      "Emirates International Equipment & Machinery L.L.C has been awarded ISO 9001:2015 Certification for its Quality Management System from Peers Quality Assurance Ltd. The certification covers the scope of supply and provision of complete solutions, including assembly, fit-out, and after-sales services related to water pumping, heating, HVAC, electrical control, and the distribution of pumps and sanitary ware. This highly regarded International Certification is a proof of our commitment to the highest standards of quality management and an affirmation of its leadership’s dedication to continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383247/1_ivy3vv.jpg",
   },
   {
     id: "iso-14001-das",
-    title: "DAS Holding has been awarded ISO 14001:2015 certificate",
+    title: "ISO 9001:2015 Certification Awarded to M.A.H.Y. KHOORY & CO., LLC",
     description: [
-      "ISO 14001:2015 certification demonstrates DAS Holding's commitment to environmental responsibility and sustainable operational practices.",
+      "M.A.H.Y. KHOORY & CO., LLC has been awarded ISO 9001:2015 Certification for its Quality Management System from Peers Quality Assurance Limited, U.K. The certification covers the scope of supply and provision of complete solutions, including Assembly, Fit out and After Sales Services of equipment and systems related to Water Pumping, Distribution, and Energy Transformation Kitchen Furniture and Accessories Distribution of Pumps, Accessories and Generation Equipment This highly regarded International Certification is a proof of our commitment to the highest standards of quality management and an affirmation of its leadership’s dedication to continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383247/2_l6tpf2.jpg",
   },
   {
     id: "iso-45001-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 9001:2015 Certification Awarded to M.A.H.Y. KHOORY & CO., LLC (Ras Al Khor Branch)",
     description: [
-      "ISO 45001 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "M.A.H.Y. KHOORY & CO., LLC has been awarded ISO 9001:2015 Certification for its Quality Management System from Peers Quality Assurance Limited, U.K.The certification covers the scope of supply and provision of complete solutions, including Assembly, Fit out and After Sales Services of equipment and systems related to: Water Pumping, Distribution, and Energy Transformation Kitchen Furniture and Accessories Distribution of Pumps, Accessories and Generation Equipment This highly regarded International Certification is a proof of our commitment to the highest standards of quality management and an affirmation of its leadership’s dedication to continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383246/3_jngoab.jpg",
   },
-   {
+  {
     id: "iso-45002-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 9001:2015 Certification Awarded to National Paper Industry (S.P.S - L.L.C)",
     description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "National Paper Industry (S.P.S - L.L.C) has been awarded ISO 9001:2015 Certification for its Quality Management System from Peers Quality Assurance Limited, U.K. This certification covers the following scope of activities: Manufacture & Supply of all ranges of Corrugated Paper Board Manufacture & Supply of Printed Boxes (100% Recyclable) This highly regarded International Certification is a proof of our commitment to the highest standards of quality management and an affirmation of its leadership’s dedication to continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383246/4_nwqqou.jpg",
   },
-   {
+  {
     id: "iso-45003-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 14001:2015 Certification Awarded to National Paper Industry (S.P.S - L.L.C)",
     description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "National Paper Industry (S.P.S - L.L.C) has been awarded ISO 14001:2015 Certification for its Environmental Management System from Peers Quality Assurance Limited, U.K.The certification covers the following scope of activities: Manufacture & Supply of all ranges of Corrugated Paper Board Manufacture & Supply of Printed Boxes (100% Recyclable) This highly regarded International Certification is a proof of our commitment to the highest standards of environmental management and an affirmation of its leadership’s dedication to sustainable practices and continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383246/5_obtdho.jpg",
   },
   {
     id: "iso-45004-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 45001:2018 Certification Awarded to National Paper Industry (S.P.S - L.L.C)",
     description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "National Paper Industry (S.P.S - L.L.C) has been awarded ISO 45001:2018 Certification for its Occupational Health & Safety Management System from Peers Quality Assurance Limited, U.K.The certification covers the following scope of activities:•	Manufacture & Supply of all ranges of Corrugated Paper Board•	Manufacture & Supply of Printed Boxes (100% Recyclable)This highly regarded International Certification is a proof of our commitment to the highest standards of occupational health and safety and an affirmation of its leadership’s dedication to providing a safe working environment and continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383246/6_zbyqkg.jpg",
   },
   {
     id: "iso-45005-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 9001:2015 Certification Awarded to PURE ENERGY CONSTRUCTION LLC",
     description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "PURE ENERGY CONSTRUCTION LLC has been awarded ISO 9001:2015 Certification for its Quality Management System from AMERICO Quality Standards Registech Pvt. Ltd.The certification covers the following scope of activities:•	Project Management, Engineering, and Procurement.•	Construction of Buildings and Electrical Fittings.•	Solar Energy Systems Installation.•	Design & Build Steel Construction.This highly regarded International Certification is a proof of our commitment to the highest standards of quality management and an affirmation of its leadership’s dedication to continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383247/7_dt6jcl.jpg",
   },
-   {
+  {
     id: "iso-45006-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 14001:2015 Certification Awarded to PURE ENERGY CONSTRUCTION LLC",
     description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "PURE ENERGY CONSTRUCTION LLC has been awarded ISO 14001:2015 Certification for its Environmental Management System from AMERICO Quality Standards Registech Pvt. Ltd.The certification covers the following scope of activities:•	Execution of Contracts in Project Management, Engineering, and Procurement.•	Construction of Buildings and Electrical Fittings.•	Solar Energy Systems Installation.•	Design & Build Steel Construction.This highly regarded International Certification is a proof of our commitment to the highest standards of environmental management and an affirmation of its leadership’s dedication to sustainable practices and continual quality improvement",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383247/8_lveavk.jpg",
   },
   {
     id: "iso-45007-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+    title: "ISO 45001:2018 Certification Awarded to PURE ENERGY CONSTRUCTION LLC",
     description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+      "PURE ENERGY CONSTRUCTION LLC has been awarded ISO 45001:2018 Certification for its Occupational Health and Safety Management System from AMERICO Quality Standards Registech Pvt. Ltd.The certification covers the following scope of activities:•	Execution of Contracts in Project Management, Engineering, and Procurement.•	Construction of Buildings and Electrical Fittings.•	Solar Energy Systems Installation.•	Design & Build Steel Construction.This highly regarded International Certification is proof of our commitment to the highest standards of occupational health and safety and an affirmation of its leadership’s dedication to providing a safe working environment and continual quality improvement.",
     ],
-    image: "/awards/ISO.png",
+    image:
+      "https://res.cloudinary.com/db3fd1qah/image/upload/v1768383246/9_j3ogxw.jpg",
   },
-  {
-    id: "iso-45008-das",
-    title: "DAS Holding has been awarded ISO 45001:2018 certificate",
-    description: [
-      "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
-    ],
-    image: "/awards/ISO.png",
-  }
+  // {
+  //   id: "iso-45008-das",
+  //   title: "DAS Holding has been awarded ISO 45001:2018 certificate",
+  //   description: [
+  //     "ISO 45002 certification reflects DAS Holding's focus on occupational health and safety management systems.",
+  //   ],
+  //   image: "/awards/ISO.png",
+  // }
 ];
