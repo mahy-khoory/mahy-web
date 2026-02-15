5a6359f0cfde242e7af02edf8d08f8b71849503a
Muhammad Bin Arshad <mbinac1199@gmail.com>
2026-02-15 23:05:24 +0500

    partners photos update

--- constants/partners.js
+++ constants/partners.js
@@ -1,13 +1,13 @@
 import { getTranslations } from "next-intl/server";
 
 const partners = [
-    { id: 0, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994346/grundfos_lbiolu.png", name: "Grundfos" },
-    { id: 1, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994347/GAIA_r8o2xz.png", name: "Gaia" },
-    { id: 2, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994347/oventrop_v6j8mf.png", name: "Oventrop" },
-    { id: 3, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994347/ListerPetter_ea6myv.png", name: "Lister Petter" },
-    { id: 4, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994347/Marathon_rqvumo.png", name: "Marathon Motor" },
-    { id: 5, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994347/Artiston_dkapb8.png", name: "Ariston" },
-    { id: 6, image: "https://res.cloudinary.com/db3fd1qah/image/upload/v1765994347/PakaWaste_ltpkeg.png", name: "Pakawaste" }
+    { id: 0, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178552/Grundfos_kie0kp.jpg", name: "Grundfos" },
+    { id: 1, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178557/partners-gaia_awpjog.jpg", name: "Gaia" },
+    { id: 2, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178553/oventop_hykzpm.jpg", name: "Oventrop" },
+    { id: 3, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178584/partners-lister-petter_jexgqd.jpg", name: "Lister Petter" },
+    { id: 4, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178586/partners-marathon-motor_zbdrme.jpg", name: "Marathon Motor" },
+    { id: 5, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178556/partners-ariston_gmgj3s.jpg", name: "Ariston" },
+    { id: 6, image: "https://res.cloudinary.com/dpn6mdpxd/image/upload/v1771178589/partners-pakawaste_tcdj3m.jpg", name: "Pakawaste" }
 ];
 
 export const getPartnerNames = async () => {
