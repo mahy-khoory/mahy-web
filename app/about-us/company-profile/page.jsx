77677e22991f01ba2535c0646bf13bd79b3f8066
Muhammad Bin Arshad <mbinac1199@gmail.com>
2026-03-27 11:59:07 +0500

    profile bg fix

--- app/about-us/company-profile/page.jsx
+++ app/about-us/company-profile/page.jsx
@@ -242,14 +242,8 @@ async function companyProfile() {
   return (
     <>
       <SubPageHeadingNoOverlayImage
-        // title={"Company Profile"}
-        // description={
-        //   "MAHY Khoory Group brings together decades of experience, deep sector knowledge, and strong operational capabilities across a diversified portfolio of businesses. Our expertise has been built over time through disciplined growth, long-term partnerships, and consistent execution across industrial, commercial, environmental, and service sectors."
-        // }
-        // // description2={
-        // //   "The Group's capabilities are anchored in physical assets, skilled teams, and integrated operations that allow us to deliver reliable solutions at scale, while maintaining high standards of governance, safety, and performance."
-        // // }
-        image={"/profile/bg2.jpg"}
+        image={"/profile/profile-bg.jpg"}
+        image2={"/profile/profile-bg-2.jpg"}
         height="h-screen"
       />
       <WhoWeAre image={"/gallery/gallery-2.jpg"} />
