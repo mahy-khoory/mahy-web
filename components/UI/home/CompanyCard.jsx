f86c5cacc56255b69977ba6a53fd435c46298539
Muhammad Bin Arshad <mbinac1199@gmail.com>
2026-04-22 18:14:18 +0500

    companies list link

--- components/UI/home/CompanyCard.jsx
+++ components/UI/home/CompanyCard.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { forwardRef, useEffect, useRef, useState } from "react";
 
 const CompanyCard = forwardRef(({ item, index, shouldPlay }, ref) => {
@@ -39,29 +40,31 @@ const CompanyCard = forwardRef(({ item, index, shouldPlay }, ref) => {
             onMouseLeave={() => setPlay(false)}
             onTouchEnd={handleTouch}
         >
-            <div className="absolute inset-0">
-                <Image src={item.image} alt={item.label} fill objectFit="cover" />
+            <Link href={item.href}>
+                <div className="absolute inset-0">
+                    <Image src={item.image} alt={item.label} fill objectFit="cover" />
 
-                <video
-                    ref={videoRef}
-                    muted
-                    loop
-                    playsInline
-                    preload="metadata"
-                    className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ${play ? "opacity-100" : "opacity-0"}`}
-                >
-                    <source src={item.video} type="video/mp4" />
-                </video>
-            </div>
-
-            <div className="absolute inset-0 z-10 bg-linear-to-t from-black/30 to-transparent">
-                <div className={`absolute bottom-5 left-0 right-0 ${play ? "translate-y-0" : "translate-y-5"} transition-all duration-300`}>
-                    <div className='flex justify-between gap-3 px-3'>
-                        <p className='md:text-sm font-medium text-gray-50'>{item.label}</p>
-                    </div>
-                    <div className='h-px w-full bg-white mt-2 -translate-x-120 group-hover:translate-x-0 transition-all duration-500' />
+                    <video
+                        ref={videoRef}
+                        muted
+                        loop
+                        playsInline
+                        preload="metadata"
+                        className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-500 ${play ? "opacity-100" : "opacity-0"}`}
+                    >
+                        <source src={item.video} type="video/mp4" />
+                    </video>
                 </div>
-            </div>
+
+                <div className="absolute inset-0 z-10 bg-linear-to-t from-black/30 to-transparent">
+                    <div className={`absolute bottom-5 left-0 right-0 ${play ? "translate-y-0" : "translate-y-5"} transition-all duration-300`}>
+                        <div className='flex justify-between gap-3 px-3'>
+                            <p className='md:text-sm font-medium text-gray-50'>{item.label}</p>
+                        </div>
+                        <div className='h-px w-full bg-white mt-2 -translate-x-120 group-hover:translate-x-0 transition-all duration-500' />
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 });
